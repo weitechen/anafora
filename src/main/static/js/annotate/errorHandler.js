991b527466aceae0e7189b27e4a57450f67b34fa
Wei-Te Chen <wech5560@verbs.colorado.edu>
2016-02-08 11:15:49 -0700

    add error handle interface

---
+++ src/main/static/js/annotate/errorHandler.js
@@ -0,0 +1,28 @@
+/*
+ * Define different level of exception
+ *
+ * */
+function WarningException(message) {
+	Error.call(message);
+	this.message = message;
+	if ("captureStackTrace" in Error)
+		Error.captureStackTrace(this, InvalidArgumentException);
+	else
+		this.stack = (new Error()).stack;
+}
+
+WarningException.prototype = Object.create(Error.prototype);
+WarningException.prototype.name = "WarningException";
+WarningException.prototype.constructor = WarningException;
+
+function ErrorException(message) {
+	Error.call(message);
+	this.message = message;
+	if ("captureStackTrace" in Error)
+		Error.captureStackTrace(this, InvalidArgumentException);
+	else
+		this.stack = (new Error()).stack;
+}
+ErrorException.prototype = Object.create(Error.prototype);
+ErrorException.prototype.name = "ErrorException";
+ErrorException.prototype.constructor = ErrorException;
