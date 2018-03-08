12f20bfee3614f717be95e0832d97a173056e627
Wei-Te Chen <weitehchen@gmail.com>
2018-03-08 10:12:31 -0700

    add url modification

--- src/main/web/urls.py
+++ src/main/web/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     #url(r'^admin/', include(admin.site.urls)),
-] + static(settings.STATIC_URL, documeent_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #urlpatterns += staticfiles_urlpatterns()
